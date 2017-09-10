8fc0cbc254285965dc5c0a380d46081d3ee0610b
liuguanbang <salodyliu@gmail.com>
2017-09-11 01:16:50 +0800

    列表

---
+++ src/数据结构与算法的Js描述/数组/createArray.js
@@ -0,0 +1,18 @@
+/**
+ * 功能描述:
+ * 2017/9/4
+ * 作者：liuguanbang
+ */
+
+const numbers = [];
+console.log('空数组的长度：' + numbers.length);
+
+const arr1 = [1,2,3,4,5];
+const arr2 = new Array(1,2,4,5);
+
+// 传入一个参数，指定数组的长度
+const arr3 = new Array(4);
+
+console.log(arr3.length);
+console.log(arr3[1]);
+console.log(arr3[10]);
